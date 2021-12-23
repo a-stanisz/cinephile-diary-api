e1e63c7fdb9dffe30362107d63a103e5524281b1
a-stanisz <hello@adamstaniszewski.com>
2021-12-23 18:01:44 +0100

    Set up db

--- movies-srv/src/server.js
+++ movies-srv/src/server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const db = require('./db-access')
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
+const { MONGO_INITDB_ROOT_USERNAME } = process.env;
+const { MONGO_INITDB_ROOT_PASSWORD } = process.env;
+const DB_HOST = 'localhost';
+const DB_PORT = 27017;
+const DB_NAME = 'test';
 
 const app = express();
 
@@ -9,6 +16,13 @@ app.get('/', (req, res, next) => {
   res.send(`Hello! It's intended to be the movie's service.`)
 });
 
+db.connect(
+  MONGO_INITDB_ROOT_USERNAME,
+  MONGO_INITDB_ROOT_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME
+);
 app.listen(PORT, HOST, () => {
-  console.log(`movies srv running at http://${HOST}:${PORT}`);
+  console.log(`movies svc running at http://${HOST}:${PORT}`);
 });
