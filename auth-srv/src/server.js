8815810c47072e9dfd8e8f4a853b513ec2d27651
a-stanisz <hello@adamstaniszewski.com>
2021-12-26 16:16:50 +0100

    Cleanup

--- auth-srv/src/server.js
+++ auth-srv/src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { authFactory, AuthError } = require("./auth");
 
-const PORT = 3000;
+const PORT = process.env.AUTH_SRV_PORT || 3000;
 const { JWT_SECRET } = process.env;
 
 if (!JWT_SECRET) {
--- movies-srv/src/server.js
+++ movies-srv/src/server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const db = require('./db-access')
+const dbConnect = require('./database');
 
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = process.env.MOVIES_SRV_PORT || 8080;
+// const HOST = '0.0.0.0';
 
-const { MONGO_INITDB_ROOT_USERNAME } = process.env;
-const { MONGO_INITDB_ROOT_PASSWORD } = process.env;
-const DB_HOST = 'localhost';
-const DB_PORT = 27017;
 const DB_NAME = 'test';
+dbConnect(DB_NAME);
 
 const app = express();
 
@@ -16,13 +13,6 @@ app.get('/', (req, res, next) => {
   res.send(`Hello! It's intended to be the movie's service.`)
 });
 
-db.connect(
-  MONGO_INITDB_ROOT_USERNAME,
-  MONGO_INITDB_ROOT_PASSWORD,
-  DB_HOST,
-  DB_PORT,
-  DB_NAME
-);
-app.listen(PORT, HOST, () => {
-  console.log(`movies svc running at http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`movies svc running at port ${PORT}`);
 });
