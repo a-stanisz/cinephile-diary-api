4fcf3ee92045712722740f9440d2f2163a5809ba
a-stanisz <hello@adamstaniszewski.com>
2021-12-26 22:12:20 +0100

    Fix typo that cause db conn err

--- movies-srv/src/database/index.js
+++ movies-srv/src/database/index.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 const {
   MONGODB_USERNAME,
   MONGODB_PASSWORD,
-  MONGODB_SRV_NAME,
+  DB_HOST,
   DB_PORT
 } = process.env;
 
 const connect = (dbName) => {
-  const connURI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_SRV_NAME}:${DB_PORT}/${dbName}?authSource=admin&readPreference=primary&ssl=false`;
+  const connURI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${DB_HOST}:${DB_PORT}/${dbName}?authSource=admin&readPreference=primary&ssl=false`;
   setTimeout(() => {
     mongoose.connect(connURI)
       .then(console.log(`connected to the database at port ${DB_PORT}`))
