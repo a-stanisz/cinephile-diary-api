f02813a764bfd28c7330e8c2ce6fde13f7a20c3d
a-stanisz <hello@adamstaniszewski.com>
2021-12-27 17:53:03 +0100

    Update external services access code

--- movies-srv/src/services/auth.js
+++ movies-srv/src/services/auth.js
@@ -1,32 +1,21 @@
 const axios = require('axios');
-// const { json } = require('express');
 const { AUTH_HOST, AUTH_PORT } = process.env;
 
-const getUserToken = (username, password) => {
-  const authURI = `http://${AUTH_HOST}:${AUTH_PORT}/auth`;
-  // const authURI = 'http://auth-srv:3002/auth'
-  // console.log(authURI);
-  const data = {
-    username: username,
-    password: password
+const getUserToken = async (username, password) => {
+  try {
+    const authURI = `http://${AUTH_HOST}:${AUTH_PORT}/auth`;
+    const reqData = {
+      username: username,
+      password: password
+    }
+    const res = await axios.post(authURI, reqData);
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+    return res.data;
+  } catch (err) {
+    console.log('Error: ', err.message);
   }
-  axios.post(authURI, data)
-  // axios({
-  //   method: 'post',
-
-  //   // headers: { 'Content-Type': 'application/json' },
-  //   url: authURI,
-
-  //   data: data
-  // })
-    .then(res => {
-      const headerDate = res.headers && res.headers.date ? res.headers.date: 'no response date';
-      console.log('Status Code:', res.status);
-      console.log('Date in Response header:', headerDate);
-      // const data = res.data;
-      // console.log(data);
-      return res.data;
-    }).catch(err => console.log('Error: ', err.message));
 }
 
 module.exports = getUserToken;
--- movies-srv/src/services/omdb.js
+++ movies-srv/src/services/omdb.js
@@ -1,16 +1,17 @@
 const axios = require('axios');
 const { OMDB_APIKEY } = process.env;
 
-const getMovieDataByTitle = (title) => {
-  const omdbURI = `http://www.omdbapi.com/?apikey=${OMDB_APIKEY}&t=${title}`
-  axios.get(omdbURI)
-    .then(res => {
-      const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
-      console.log('Status Code:', res.status);
-      console.log('Date in Response header:', headerDate);
-      return res.data;
-    })
-    .catch(err => console.log('Error: ', err.message));
+const getMovieDataByTitle = async (title) => {
+  try {
+    const omdbURI = `http://www.omdbapi.com/?apikey=${OMDB_APIKEY}&t=${title}`;
+    const res = await axios.get(omdbURI);
+    const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+    console.log('Status Code:', res.status);
+    console.log('Date in Response header:', headerDate);
+    return res.data;
+  } catch (err) {
+    console.log('Error: ', err.message);
+  }
 }
 
 module.exports = getMovieDataByTitle;
