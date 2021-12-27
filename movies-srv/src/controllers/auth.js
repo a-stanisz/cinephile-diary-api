3b8c792ca549808463b8d66d94e7760ab9fd3b75
a-stanisz <hello@adamstaniszewski.com>
2021-12-28 00:39:24 +0100

    Add controllers

---
+++ movies-srv/src/controllers/auth.js
@@ -0,0 +1,10 @@
+const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+
+// const User = require('../models/user');
+
+const { JWT_SECRET } = process.env;
+
+exports.signIn = async (req, res, next) => {
+  
+}
--- movies-srv/src/controllers/diary.js
+++ movies-srv/src/controllers/diary.js
@@ -1,9 +1,38 @@
-const DiaryRecord = ""
-const User = ""
+// const DiaryRecord = require('../models/movie');
+const User = require('../models/user');
+const Movie = require('../models/movie');
 
 exports.postMovie = async (req, res, next) => {
+  if (!req.user || !req.movieData) {
+    const error = new Error('Not Found!');
+    error.statusCode = 404;
+    throw error;
+  }
+  const userId = req.userId;
+  const userName = req.userName;
+  const userRole = req.userRole;
   try {
-
+    let user;
+    user = await User.findOne({userId: userId});
+    if (!user) {
+      user = new User({
+        userId: userId,
+        userName: userName,
+        userRole: userRole,
+      });
+      await user.save();
+      console.log('Created new user');
+    };
+    user = await User.findOne({userId: userId});
+    const movieEntry = new Movie({
+      title: req.movieData.title,
+      releaseDate: req.movieData.releaseDate,
+      genre: req.movieData.genre,
+      director: req.movieData.director,
+    });
+    user.diaryEntries.push(movieEntry);
+    await user.save();
+    res.status(200).json({message: 'Database updated!'});
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
