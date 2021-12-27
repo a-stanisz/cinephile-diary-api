77b7fa19f1e378d74697284700d04c1240d17d36
a-stanisz <hello@adamstaniszewski.com>
2021-12-28 00:38:59 +0100

    Add models

--- movies-srv/src/models/movie.js
+++ movies-srv/src/models/movie.js
@@ -1,15 +1,39 @@
-class Movie {
-  constructor(title, releasedDate, genre, director) {
-    this.data = { title, releasedDate, genre, director }
-  }
+// class Movie {
+//   constructor(title, releasedDate, genre, director) {
+//     this.data = { title, releasedDate, genre, director }
+//   }
   
-}
+// }
 
-let hobbit = new Movie(
-  'The Hobbit: An Unexpected Journey',
-  new Date('2012-12-14'),
-  'Adventure, Fantasy',
-  'Peter Jackson'
-)
+// let hobbit = new Movie(
+//   'The Hobbit: An Unexpected Journey',
+//   new Date('2012-12-14'),
+//   'Adventure, Fantasy',
+//   'Peter Jackson'
+// )
 
-console.log(hobbit.data);
+// // console.log(hobbit.data);
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const entrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    releaseDate: {
+      type: Date,
+    },
+    genre: {
+      type: String,
+    },
+    director: {
+      type: String,
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('Movie', entrySchema);
--- movies-srv/src/models/user.js
+++ movies-srv/src/models/user.js
@@ -1,9 +1,40 @@
-class User {
-  constructor(_id, name, role) {
-    this.data = { _id, name, role }
-  }
-  get role() {
-    return this.data.role;
-  }
+// class User {
+//   constructor(_id, name, role) {
+//     this.data = { _id, name, role }
+//   }
+//   get role() {
+//     return this.data.role;
+//   }
 
-}
+// }
+
+// const { Int32 } = require('mongodb');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Number,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    userRole: {
+      type: String,
+      required: true,
+    },
+    diaryEntries: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Movie',
+    }]
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
+
+
+// console.log(Object.keys(userSchema.schema.paths));
