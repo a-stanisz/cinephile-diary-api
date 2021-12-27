b11c82b4c93be10aa54b50fbf62da979ae44f2af
a-stanisz <hello@adamstaniszewski.com>
2021-12-28 00:35:31 +0100

    Update with some work-in-progress code

--- movies-srv/src/server.js
+++ movies-srv/src/server.js
@@ -1,21 +1,89 @@
 const express = require('express');
-const dbConnect = require('./database');
+const dbConnect = require('./database/connect');
+
+const DB_NAME = 'movies';
+dbConnect(DB_NAME);
 
 const PORT = process.env.MOVIES_SRV_PORT || 8080;
 
-const DB_NAME = 'test';
-dbConnect(DB_NAME);
+// Retrieve valid tokens:
+const auth = require('./services/auth');
+const getTokens = async () => {
+  try {
+    const basicUser = {
+      username: 'basic-thomas',
+      password: 'sR-_pcoow-27-6PAwCD8',
+      }
+    const premiumUser = {
+      username: 'premium-jim',
+      password: 'GBLtTyq3E_UNjFnpo9m6',
+    }
+    basicUser.token = await auth(basicUser.username, basicUser.password);
+    console.log(basicUser);
+    premiumUser.token = await auth(premiumUser.username, premiumUser.password);
+    console.log(premiumUser);
+    } catch (err) {
+      console.log('Error: ', err.message);
+    }
+}
+getTokens();
 
-// Test other modules if work:
+
+
+// const movieProps = [
+//   'title',
+//   'releaseDate',
+//   'genre',
+//   'director'
+// ];
+
 // const omdb = require('./services/omdb');
-// omdb('Hobbit');
+// omdb('The Last Duel', [...movieProps])
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
+  
+// const auth = require('./services/auth');
+const jwt = require('jsonwebtoken');
+// const bcrypt = require('bcryptjs');
+const { JWT_SECRET } = process.env;
 
-const authsrv = require('./services/auth');
-authsrv('basic-thomas', 'sR-_pcoow-27-6PAwCD8');
+const decode = async () => {
+  let decodedToken;
+  try {
+    const token = await auth('basic-thomas', 'sR-_pcoow-27-6PAwCD8');
+    decodedToken = jwt.verify(String(token.token), JWT_SECRET)
+    // console.log(typeof String(token.token), String(token.token));
+  } catch (err) {
+    console.log('Error: ', err.message);
+  }
+  console.log(decodedToken.userId);
+}
+
+// decode();
+  // .then(res => console.log(res))
+  // .catch(err => console.log(err));
+  
+
+// const getDecodedToken = async () => {
+//   let decodedToken;
+//   try {
+//     decodedToken = jwt.verify()
+//   } catch (err) {
+//     console.log('Error: ', err.message);
+//   }
+// }
+
+// const token = require('./middleware/isAuth');
+// token();
+
+
 
 // 
-
+const diaryRoutes = require('./routes/diary');
+const isAuth = require('./middleware/isAuth');
 const app = express();
+
+app.use('/', diaryRoutes);
 
 app.get('/', (req, res, next) => {
   res.send(`Hello! It's intended to be the movie's service.`)
@@ -24,3 +92,6 @@ app.get('/', (req, res, next) => {
 app.listen(PORT, () => {
   console.log(`movies svc running at port ${PORT}`);
 });
+
+// const resul2 = authsrv.getUserToken('basic-thomas', 'sR-_pcoow-27-6PAwCD8');
+// console.log(typeof resul2, resul2);
