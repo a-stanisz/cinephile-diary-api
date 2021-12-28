397a52246b7d1c27699f57287e082c2eeb01d95e
a-stanisz <hello@adamstaniszewski.com>
2021-12-29 00:31:19 +0100

    Update database connection code

--- movies-srv/src/database/connect.js
+++ movies-srv/src/database/connect.js
@@ -1,19 +1,22 @@
 const mongoose = require('mongoose');
 
+mongoose.set('debug', true);
+
 const {
   MONGODB_USERNAME,
   MONGODB_PASSWORD,
   DB_HOST,
-  DB_PORT
+  DB_PORT,
+  DB_NAME,
 } = process.env;
 
-const connect = (dbName) => {
-  const connURI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${DB_HOST}:${DB_PORT}/${dbName}?authSource=admin&readPreference=primary&ssl=false`;
-  setTimeout(() => {
-    mongoose.connect(connURI)
-      .then(console.log(`connected to the database at port ${DB_PORT}`))
-      .catch(err => console.log(err));
-  }, 2000);
+const connect = async () => {
+  try {
+    const connURI = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin&readPreference=primary&ssl=false`;
+    await mongoose.connect(connURI);
+  } catch(err) {
+    console.error(err);
+  }
 }
 
 module.exports = connect;
---
+++ movies-srv/src/database/seed.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+const users = [
+  {
+    id: 123,
+    role: "basic",
+    name: "Basic Thomas",
+    username: "basic-thomas",
+    password: "sR-_pcoow-27-6PAwCD8",
+  },
+  {
+    id: 434,
+    role: "premium",
+    name: "Premium Jim",
+    username: "premium-jim",
+    password: "GBLtTyq3E_UNjFnpo9m6",
+  },
+];
+
+// TODO: Figure out if it is right to create a User on request or maybe it is needed to populate db in advance on svc startup?
+
