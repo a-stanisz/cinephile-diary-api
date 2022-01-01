d2af9604b142e1e231844479b43ade0dae4a5d24
a-stanisz <hello@adamstaniszewski.com>
2022-01-01 21:00:17 +0100

    Test OMDB API call

--- movies-srv/src/services/omdb.js
+++ movies-srv/src/services/omdb.js
@@ -5,7 +5,7 @@ const getMovieDataByTitle = async (title) => {
   try {
     const omdbURI = `http://www.omdbapi.com/?apikey=${OMDB_APIKEY}&t=${title}`;
     const res = await axios.get(omdbURI);
-    console.log('OMDB API GET Status Code:', res.status);
+    // console.log('OMDB API GET Status Code:', res.status);
     return res.data;
   } catch (err) {
     console.log('Error: ', err.message);
