52eeaa315125de3486d8fbfe62a83f2dbb5f1054
a-stanisz <hello@adamstaniszewski.com>
2022-01-02 19:46:35 +0100

    remove unnecessary code

--- movies-srv/src/server.js
+++ movies-srv/src/server.js
@@ -4,20 +4,6 @@ const resetLimitCounters = require('./workers/resetCounter');
 
 dbConnect();
 
-// Provide fresh tokens to copy into sample requests for testing endpoints:
-const auth = require('./services/auth');
-(async () => {
-  try {
-    const basicUsrToken = await auth('basic-thomas', 'sR-_pcoow-27-6PAwCD8');
-    const premiumUsrToken = await auth('premium-jim', 'GBLtTyq3E_UNjFnpo9m6');
-    console.log('\nCopy following token for testing purposes: ', "'basic-thomas': ", basicUsrToken);
-    console.log('Copy following token for testing purposes: ', "'premium-jim': ", premiumUsrToken);
-  } catch (err) {
-    console.error(err);
-  }
-})();
-// 
-
 const PORT = process.env.MOVIES_SRV_PORT || 8080;
 
 const diaryRoutes = require('./routes/diary');
